cae33cc7d0528908466054187f9f0cc0a4616d8e
Simon Lacombe <simon.lacombe@ens-lyon.fr>
2022-04-27 11:00:41 +0200

    script to plot outputs from the model

--- src/model_analysis.R
+++ src/model_analysis.R
@@ -9,6 +9,7 @@ sapply(c('stringr', 'foreach',
        function(x) suppressPackageStartupMessages(require(x , character.only = TRUE, quietly = TRUE)))
 
 rm(list=ls())
+source("src/model_analysis_utility_functions.R")
 
 years_1 <- as.character(2006:2016)
 years_2 <- as.character(2017:2021)
@@ -105,9 +106,9 @@ for(i in unique(M.sum$param)){
 
 M.sum[M.sum$param %in% c("rho_bait", "yr_rho"),]$param <- "rho"
 
-#########PLOT############
+#########PLOT VALUES############
 
-ggplot(M.sum[M.sum$species == "ArcticFox",])+
+ggplot(M.sum[M.sum$species == "RedFox",])+
   geom_hline(yintercept = 0, color = "black", size = 1) +
   geom_pointrange(aes(x = cov, y = mean, ymin=lower, ymax=upper, color = model), 
                   position = position_jitterdodge(), show.legend = F, size = .8)+
@@ -132,3 +133,49 @@ ggplot(M.sum[M.sum$species == "ArcticFox",])+
         legend.text = element_text(face = "bold", size = 11),
         legend.title=element_text(face = "bold", size = 11))+
   guides(colour = guide_legend(title.position="top", title.hjust = 0))
+
+### PLOT PROBAS -----
+covs1 <- matrix(0, nrow= 100, ncol = 4)
+covs1[,1] <- 1
+covs1[,3] <- seq(-2,2,length.out=100)
+
+param.df <- data.frame(param = rep(c("colonization"), 400),
+                       species = rep(c("Arctic Fox"), 400),
+                       cond= rep(c("absent", "present"), each = 100,2),
+                       FTG = rep(covs1[,3],4),
+                       model = rep(c(1,2), each = 200))%>%
+  as.data.table()
+
+param.df$lower <- 0
+param.df$m <- 0
+param.df$upper <- 0
+
+preds_gam_1 <- make_preds_summ(M.mat_1, sp = 2, p1 = "b", p2 = "g", xp1 = covs1, xp2=covs1[,1])
+preds_gam_2 <- make_preds_summ(M.mat_2, sp = 2, p1 = "b", p2 = "g", xp1 = covs1, xp2=covs1[,1])
+
+param.df[param == "colonization"&species=="Arctic Fox"&model==1]$lower <-c(preds_gam_1[["wo"]][1,], preds_gam_1[["w"]][1,])
+param.df[param == "colonization"&species=="Arctic Fox"&model==1]$m <-c(preds_gam_1[["wo"]][2,], preds_gam_1[["w"]][2,])
+param.df[param == "colonization"&species=="Arctic Fox"&model==1]$upper <-c(preds_gam_1[["wo"]][3,], preds_gam_1[["w"]][3,])
+
+param.df[param == "colonization"&species=="Arctic Fox"&model==2]$lower <-c(preds_gam_2[["wo"]][1,], preds_gam_2[["w"]][1,])
+param.df[param == "colonization"&species=="Arctic Fox"&model==2]$m <-c(preds_gam_2[["wo"]][2,], preds_gam_2[["w"]][2,])
+param.df[param == "colonization"&species=="Arctic Fox"&model==2]$upper <-c(preds_gam_2[["wo"]][3,], preds_gam_2[["w"]][3,])
+
+#########PLOT PROBAS############
+
+ggplot(param.df[param.df$cond=="absent",])+
+  geom_line(aes(x=FTG,y=m, color = factor(model)), size = 1)+
+  geom_ribbon(aes(x=FTG,y=m, ymin = lower, ymax = upper, fill = factor(model)), alpha = .2, show.legend = FALSE)+
+  scale_color_manual(values = c("#00AFBB", "#FC4E07"))+
+  scale_fill_manual(values = c("#00AFBB", "#FC4E07"))+
+  xlab("FTG")+ylab("colonization probability")+labs(col="Model")+
+  theme(panel.background = element_rect(fill = "white"),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color="grey" ),
+        strip.text = element_text(face = "bold"),
+        axis.text.x = element_text(face = "bold"),
+        axis.title.x = element_text(size=8,face = "bold"),
+        axis.title.y = element_text(size=8,face = "bold"),
+        axis.ticks = element_blank())
